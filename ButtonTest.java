54318aa057a26c91d71ab8ad2d5863199d45918f
yaranandes7131 <124922751+yaranandes7131@users.noreply.github.com>
2023-03-09 14:17:40 -0400

    Add files via upload

---
+++ ButtonTest.java
@@ -0,0 +1,16 @@
+package javaapplication7;
+
+import javax.swing.JFrame;
+
+public class ButtonTest
+{
+    public static void main(String[] args)
+    {
+        javaapplication7 buttonFrame = new javaapplication7();
+        buttonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        buttonFrame.setSize(275, 110);
+        buttonFrame.setVisible(true);
+       
+    }
+}
+        
---
+++ javaapplication7.java
@@ -0,0 +1,46 @@
+package javaapplication7;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class javaapplication7 extends JFrame
+{
+
+    private JButton plainJButton;
+    private JButton fancyButton;
+    
+    
+    public javaapplication7()
+    {
+        super("Testing Buttons");
+        setLayout( new FlowLayout() );
+        
+        plainJButton = new JButton("Plain Button");
+        add( plainJButton);
+        
+       Icon bug1 = new ImageIcon( getClass().getResource( "bug1.gif"));
+       Icon bug2 = new ImageIcon( getClass().getResource( "bug2.gif"));
+       fancyButton = new JButton( "Fancy Button", bug1);
+       fancyButton.setRolloverIcon(bug2);
+       add(fancyButton);
+       
+       ButtonHandler handler = new ButtonHandler();
+       fancyButton.addActionListener( handler);
+       plainJButton.addActionListener( handler);
+       
+    }
+        private class ButtonHandler implements ActionListener
+        {
+            @Override
+            public void actionPerformed(ActionEvent event)
+            {
+                JOptionPane.showMessageDialog(null, String.format("You pressed: %s", event.getActionCommand()));
+            }
+        }
+
+}
